347a439f8003200cf548c84b52636f030b8af843
JCHSS <136691850@qq.com>
2016-01-06 09:22:30 +0800

    新的工程

---
+++ NewTest/NewTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NewTest
+//
+//  Created by MS on 16-1-6.
+//  Copyright (c) 2016年 MS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NewTest/NewTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NewTest
+//
+//  Created by MS on 16-1-6.
+//  Copyright (c) 2016年 MS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
